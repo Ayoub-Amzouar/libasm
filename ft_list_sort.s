9e2f3ff0fa1b396baec4d07b03ef01d9875bd3fc
Ayoub-Amzouar <gramgeek01@gmail.com>
2021-03-10 15:39:22 +0100

    tryin to understand what hamza did

--- ft_list_sort.s
+++ ft_list_sort.s
@@ -5,6 +5,7 @@
 ; rdi: *head
 ; rdx: pointer to a compare function
 ; rcx: (*head)->next
+; r8: tmp variable
 
 _ft_list_sort:
 	mov	rdi, [rdi]
@@ -15,28 +16,36 @@ loop1:
 	je return_value
 	mov rcx, qword [rdi+8]
 	jmp loop2
-	mov rdi, qword [rdi+8]
-	jmp loop1
 
 loop2:
 	cmp rcx, 0
-	je loop1
+	je replay
 	push rdi
 	push rcx
 	push rdx
 	mov rdi, qword [rdi]
 	mov rsi, qword [rcx]
 	call rdx
-	cmp rax, 0
-	jg swap
+	cmp rax, 1
 	pop rdx
 	pop rcx
 	pop rdi
+	jz swap
 	mov rcx, qword [rcx+8]
 	jmp loop2
 
 swap:
+	push qword [rdi] ; we push rdi which is *head to save its value
+	mov r8, qword [rcx] ; we put the address of the member data
+	mov qword [rdi], r8 ; we put that address into the (*head)->data 
+	pop r8
+	mov qword [rcx], r8
+	mov rcx, qword [rcx+8] ; tmp = tmp->next
+	jmp loop2
 
+replay:
+	mov rdi, qword [rdi+8] ; (*head) = (*head)->next
+	jmp loop1
 
 return_value:
 	ret
